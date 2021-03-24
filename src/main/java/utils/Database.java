c4936e598838f9023013b12960a359fd24de4dc3
mr6641989 <mr664@uowmail.edu.au>
2021-03-24 12:00:25 +1100

    Fix database

--- src/main/java/utils/Database.java
+++ src/main/java/utils/Database.java
@@ -6,8 +6,8 @@ import com.j256.ormlite.support.ConnectionSource;
 import java.sql.SQLException;
 
 public class Database {
-   // public final static String databaseUrl = "jdbc:sqlite:C:\\Users\\matth\\IdeaProjects\\allinsureapp\\allinsure.db";
-   public final static String databaseUrl = "jdbc:mysql://us-cdbr-iron-east-05.cleardb.net/heroku_e4ec4ac28a51ba7?reconnect=true";
+   public final static String databaseUrl = "jdbc:sqlite:C:\\Users\\matth\\IdeaProjects\\allinsureapp\\allinsure.db";
+   //public final static String databaseUrl = "jdbc:mysql://us-cdbr-iron-east-05.cleardb.net/heroku_e4ec4ac28a51ba7?reconnect=true";
 
     public static ConnectionSource databaseConn() throws SQLException {
         // create a connection source to our database
